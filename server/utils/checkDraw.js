fc8c58aac8c8e6cf96868945c384474e750e6b17
Johan Costa <159167858+johancosta999@users.noreply.github.com>
2026-05-30 14:28:04 +0530

    add checkDraw utility function to determine if the game is a draw

---
+++ server/utils/checkDraw.js
@@ -0,0 +1,15 @@
+const checkDraw = (board) => {
+
+    for (let row = 0; row < 4; row++){
+        
+        for (let col = 0; col <4; col++){
+            if(board[row][col] === null){
+                return false;
+            }
+        }
+    }
+
+    return true;
+};
+
+module.exports = {checkDraw}
