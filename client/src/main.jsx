6515abcee576209d88357dbcdf8cc9c1240e1b13
Johan Costa <159167858+johancosta999@users.noreply.github.com>
2026-06-01 23:28:59 +0530

    feat: wrap App component with GameProvider for state management

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { GameProvider } from "./context/GameProvider";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <GameProvider>
+      <App />
+    </GameProvider>
+  </StrictMode>
+);
